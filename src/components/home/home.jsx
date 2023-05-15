3d47f48e28b3f03f96427f3175a5a0584cf35db1
Marcus Sandi <tradedomarcus@gmail.com>
2023-05-15 08:04:38 -0300

    feat: add home component and content

---
+++ src/components/home/home.jsx
@@ -0,0 +1,13 @@
+import { ContainerHome, MainTitle } from "./home.styles"
+
+const Home = () => {
+    return (
+        <ContainerHome>
+            <MainTitle>Site para sua marca</MainTitle>
+            <p>Crie seu site e seja conhecido por todos!</p>
+            <h2>Sites por menos de R$ 500,00 !!</h2>
+        </ContainerHome>
+    )
+}
+
+export default Home
