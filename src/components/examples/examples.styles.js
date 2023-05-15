c52818e6c28b770026f38d21fdc52e402b9761a3
Marcus Sandi <tradedomarcus@gmail.com>
2023-05-15 08:07:42 -0300

    feat: styles of example component

---
+++ src/components/examples/examples.styles.js
@@ -0,0 +1,17 @@
+import { styled } from "styled-components";
+
+export const Card = styled.section`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	margin: 0 auto;
+`;
+
+export const InnerContentCard = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	margin: 0 auto;
+	flex-direction: column;
+	margin: auto 20%;
+`;
