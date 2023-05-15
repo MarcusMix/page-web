cab8a7d572e201a1369cae636b97bc0fa7bf7417
Marcus Sandi <tradedomarcus@gmail.com>
2023-05-15 08:13:18 -0300

    feat: add render components

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Examples from './components/examples/examples'
+import Home from './components/home/home'
+import HowWorks from './components/how-works/howWorks'
+
+function App() {
+
+  return (
+    <>
+      <Home/>
+      <HowWorks/>
+      <Examples/>
+    </>
+  )
+}
+
+export default App
