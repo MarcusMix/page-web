ec85d70c492c44712dffc33083e187f40469cfda
Marcus Sandi <tradedomarcus@gmail.com>
2023-05-15 08:07:23 -0300

    feat: add examples of other projects

---
+++ src/components/examples/examples.jsx
@@ -0,0 +1,21 @@
+import { Card, InnerContentCard } from "./examples.styles";
+
+const Examples = () => {
+	return (
+		<Card>
+			<InnerContentCard>
+                <h3>exemplo 1</h3>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing
+					elit. Animi nesciunt temporibus dolores quaerat, a
+					consectetur ex, pariatur vero nostrum, eligendi
+					praesentium odio. Cumque, maxime ea quae inventore
+					nulla ad dolorum!
+				</p>
+				<h4>imagem</h4>
+			</InnerContentCard>
+		</Card>
+	);
+};
+
+export default Examples;
