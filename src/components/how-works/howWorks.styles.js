29ff2761eab81055e3dd1a708a846c8bb386cbeb
Marcus Sandi <tradedomarcus@gmail.com>
2023-05-15 08:07:10 -0300

    feat: styles how works component

---
+++ src/components/how-works/howWorks.styles.js
@@ -0,0 +1,14 @@
+import { styled } from "styled-components";
+
+export const TitleHow = styled.h2 `
+    text-align: center;
+`
+
+export const Wrapper = styled.div `
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width: 500px;
+    margin: 0 auto;
+`
