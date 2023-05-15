a7fd806f3a25cd757871ab31c117321a5a6b3473
Marcus Sandi <tradedomarcus@gmail.com>
2023-05-15 08:04:48 -0300

    feat: add home styles

---
+++ src/components/home/home.styles.js
@@ -0,0 +1,12 @@
+import { styled } from "styled-components";
+
+export const ContainerHome = styled.div `
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`
+
+export const MainTitle = styled.h1`
+	/* font-size: 50px; */
+`;
