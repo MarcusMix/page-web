7bd019e74f89c5026b282def06269f39ef6ceaf7
Marcus Sandi <tradedomarcus@gmail.com>
2023-05-17 08:30:53 -0300

    feat: add why create a site

---
+++ src/components/why/why.jsx
@@ -0,0 +1,12 @@
+import { WhyContainer } from './why.styles'
+
+const Why = () => {
+    return (
+        <WhyContainer id='why'>
+            <h2>Por que ter um site?</h2>
+
+        </WhyContainer>
+    )
+}
+
+export default Why
