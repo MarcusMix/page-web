0f3891e305f5313688536e3cf55fa4e1e75015ba
Marcus Sandi <tradedomarcus@gmail.com>
2023-05-17 08:33:09 -0300

    feat: setup react router and render pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Projects from "./pages/projects/projects.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<App />} />
+				<Route path="/projects" element={<Projects />} />
+			</Routes>
+		</BrowserRouter>
+	</React.StrictMode>
+);
