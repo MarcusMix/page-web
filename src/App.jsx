7df4a36180524de56b8af7d410ac32c6e44f7cef
Marcus Sandi <tradedomarcus@gmail.com>
2023-05-17 08:32:51 -0300

    feat: render navbar and why components

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import './App.css'
 import Examples from './components/examples/examples'
 import Home from './components/home/home'
 import HowWorks from './components/how-works/howWorks'
+import Why from './components/why/why'
+import Navbar from './components/navbar/navbar'
 
 function App() {
 
   return (
     <>
+    <Navbar/>
       <Home/>
+      <Why/>
       <HowWorks/>
       <Examples/>
     </>
