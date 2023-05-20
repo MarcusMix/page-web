148ec85d0e01b28b4f2c8d88d922b67778b55bc9
Marcus Sandi <tradedomarcus@gmail.com>
2023-05-20 14:34:43 -0300

    feat: add icons

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,13 +1,37 @@
-import { ContainerHome, MainTitle } from "./home.styles"
+import { ContainerHome, MainTitle } from "./home.styles";
+
+import { FaMoneyBillWave, FaPencilAlt } from "react-icons/fa";
+import { AiOutlineGlobal } from "react-icons/ai";
+import { BsRocketTakeoffFill } from "react-icons/bs";
+import { IoMdHeart } from "react-icons/io";
+
+import Image from '../../assets/edited.png'
 
 const Home = () => {
-    return (
-        <ContainerHome>
-            <MainTitle>Site para sua marca</MainTitle>
-            <p>Crie seu site e seja conhecido por todos!</p>
-            <h2>Sites por menos de R$ 500,00 !!</h2>
-        </ContainerHome>
-    )
-}
+	return (
+		<ContainerHome>
+			<MainTitle>Tenha seu site em pouco tempo!</MainTitle>
+			<ul>
+				<li>
+					<AiOutlineGlobal /> Crie seu site e seja conhecido
+					por todos!
+				</li>
+				<li>
+					<BsRocketTakeoffFill /> Rápido, fácil e seguro!
+				</li>
+				<li>
+					<IoMdHeart /> Do jeitinho que você precisa.
+				</li>
+				<li>
+					<FaMoneyBillWave /> Preço que cabe no seu bolso!
+				</li>
+				<li>
+					<FaPencilAlt /> 100% personalizado.
+				</li>
+			</ul>
+            <img src={Image} alt="" />
+		</ContainerHome>
+	);
+};
 
-export default Home
+export default Home;
--- src/components/home/home.styles.js
+++ src/components/home/home.styles.js
@@ -1,13 +1,49 @@
 import { styled } from "styled-components";
 
-export const ContainerHome = styled.div `
-    display: flex;
-    /* justify-content: center; */
-    align-items: center;
-    flex-direction: column;
-    height: 100vh;
-`
+export const ContainerHome = styled.div`
+	display: flex;
+	/* justify-content: center; */
+	align-items: center;
+	flex-direction: column;
+	height: 100vh;
+
+	& img {
+		padding: 1rem;
+		max-width: 30%;
+        margin: 3rem auto;
+
+		@media (max-width: 768px) {
+			max-width: 75%;
+			margin: auto;
+		}
+	}
+
+	& ul {
+		display: flex;
+		flex-direction: column;
+		list-style: none;
+		align-items: center;
+		padding: 0.5rem;
+
+		& li {
+			padding: 0.25rem;
+			text-align: center;
+			font-size: 20px;
+			color:  #FED766;
+		}
+
+		& svg {
+			vertical-align: -10%;
+		}
+	}
+`;
 
 export const MainTitle = styled.h1`
-	/* font-size: 50px; */
+	font-size: 50px;
+	color: #f2f2f2;
+    padding: 1.5rem;
+
+	@media (max-width: 768px) {
+		font-size: 35px;
+	}
 `;
