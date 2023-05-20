b5e4df49229ba1510a2318dc90fb0feff79db130
Marcus Sandi <tradedomarcus@gmail.com>
2023-05-20 11:10:33 -0300

    refactor: change app to home page

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Projects from "./pages/projects/projects.jsx";
+import Homepage from "./pages/home-page/home-page.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<App />} />
+				<Route path="/" element={<Homepage />} />
 				<Route path="/projects" element={<Projects />} />
 			</Routes>
 		</BrowserRouter>
