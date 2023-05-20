8e77b8d8a54db82d89f390f2e0abd0b512589b8e
Marcus Sandi <tradedomarcus@gmail.com>
2023-05-20 14:35:55 -0300

    feat: styles and motivation

--- src/components/why/why.jsx
+++ src/components/why/why.jsx
@@ -4,7 +4,10 @@ const Why = () => {
     return (
         <WhyContainer id='why'>
             <h2>Por que ter um site?</h2>
-
+            <h3>Aumente suas vendas</h3>
+            <h3>Ganhe visibilidade</h3>
+            <h3>Tenha mais clientes</h3>
+            <h3>Mostre seu trabalho para todos</h3>
         </WhyContainer>
     )
 }
--- src/components/why/why.styles.js
+++ src/components/why/why.styles.js
@@ -4,6 +4,17 @@ export const WhyContainer = styled.div`
 	display: flex;
 	align-items: center;
 	justify-content: center;
+	flex-direction: column;
 	height: 100vh;
-	background-color: aqua;
+	background-color: #798478;	
+
+	& h2 {
+		font-size: 40px;
+		margin: 1rem;
+		color: #11151C;
+	}
+
+	& h3 {
+		margin: .25rem;
+	}
 `;
