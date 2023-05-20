68b7bbaa37037b16850a0b6f5e05d6e7aec4b59f
Marcus Sandi <tradedomarcus@gmail.com>
2023-05-20 14:35:30 -0300

    feat: change color, add anchor and options

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,13 +1,27 @@
 import { NavbarContainer, NavbarItems } from "./navbar.styles";
 import { NavLink } from "react-router-dom";
 
+import { HiOutlineGlobeAlt } from "react-icons/hi";
+
 const Navbar = () => {
 	return (
 		<NavbarContainer>
+			<h1>
+				<HiOutlineGlobeAlt /> Site para sua Marca
+			</h1>
 			<NavbarItems>
-				<p><NavLink to="/projects">Projetos</NavLink></p>
-				<p><a href="#why">Peça o seu</a></p>
-				<p><a href="#how">Como funciona</a></p>
+				<p>
+					<NavLink to="/projects">Projetos</NavLink>
+				</p>
+				<p>
+					<a href="#why">Peça o seu</a>
+				</p>
+				<p>
+					<a href="#how">Como funciona</a>
+				</p>
+				<p>
+					<a href="#contact">Contate-nos</a>
+				</p>
 			</NavbarItems>
 		</NavbarContainer>
 	);
--- src/components/navbar/navbar.styles.js
+++ src/components/navbar/navbar.styles.js
@@ -6,17 +6,27 @@ export const NavbarContainer = styled.nav`
 	justify-content: space-between;
 	/* width: 100vw; */
 	height: 80px;
-	background-color: blue;
+	background-color: #fb6107;
+	/* #777DA7 */
+
+	& svg {
+		vertical-align: -10%;
+	}
 `;
 
 export const NavbarItems = styled.div`
 	display: flex;
 	flex-direction: row;
 	list-style-type: none;
-    
 
-    & a {
-        color: #fff;
-        padding: 15px;
-    }
+	& a {
+		color: #f2f2f2;
+		padding: 15px;
+		text-decoration: none;
+	}
+
+	:hover {
+		text-decoration: underline;
+		color: #FED766;
+	}
 `;
