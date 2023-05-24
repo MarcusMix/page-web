085434f95a5051fa69d6d4f1b6a719c8da47a273
Marcus Sandi <tradedomarcus@gmail.com>
2023-05-24 08:34:28 -0300

    feat: styling card, steps and wrapper

--- src/components/how-works/howWorks.styles.js
+++ src/components/how-works/howWorks.styles.js
@@ -1,17 +1,50 @@
 import { styled } from "styled-components";
 
-export const TitleHow = styled.h2 `
-    text-align: center;
-    color: #FB6107;
-    font-size: 40px;
-`
+export const TitleHow = styled.h2`
+	text-align: center;
+	color: #fb6107;
+	font-size: 40px;
+`;
 
-export const Wrapper = styled.div `
-    display: flex;
-    justify-content: center;
-    align-items: center;
+export const Wrapper = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+	margin: auto;
+
+	p {
+		font-size: 20px;
+        text-align: center;
+	}
+`;
+
+export const Steps = styled.section`
+	display: flex;
+	align-items: center;
     flex-direction: column;
-    width: 500px;
-    margin: 0 auto;
-    height: 100vh;
-`
+
+`;
+
+export const CardInfo = styled.div`
+	display: flex;
+    border-radius: 2rem;
+	flex-direction: column;
+	align-items: center;
+    justify-content: space-evenly;
+	transition: all 300ms;
+    background-color: grey;
+    width: 500px; 
+    height: 400px;
+    padding: 1rem;
+    
+	:hover {
+        /* opacity: 0.8; */
+        transition: all 300ms;
+		transform: scale(1.1);
+	}
+
+    h4 {
+        font-size: 30px;
+    }
+`;
