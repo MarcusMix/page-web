79b3defdda3d7d14d6b6ee7db7ceac2462fbbd9b
Marcus Sandi <tradedomarcus@gmail.com>
2023-06-03 12:01:02 -0300

    refactor: remove useState and other things about menu hamburguer

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,32 +1,29 @@
-import { NavbarContainer, NavbarItems, MobileMenu } from "./navbar.styles";
-import { NavLink } from "react-router-dom";
+import { NavbarContainer, NavbarItems } from "./navbar.styles";
 
 import { HiOutlineGlobeAlt } from "react-icons/hi";
-import { RxHamburgerMenu } from "react-icons/rx";
-import { GrFormClose } from "react-icons/gr";
+// import { RxHamburgerMenu } from "react-icons/rx";
+// import { GrFormClose } from "react-icons/gr";
 
-import { useState } from "react";
+// import { useState } from "react";
 
-import Sidebar from "../sidebar/sidebar";
+// import Sidebar from "../sidebar/sidebar";
 
 const Navbar = () => {
-	const [sidebar, setSidebar] = useState(false);
+	// const [sidebar, setSidebar] = useState(true);
 
-	const showSidebar = () => setSidebar(!sidebar);
+	// const showSidebar = () => setSidebar(!sidebar);
 
 	return (
 		<NavbarContainer>
-			<MobileMenu>
-				{sidebar ? <RxHamburgerMenu size={30} onClick={showSidebar}/> : <GrFormClose size={40} onClick={showSidebar} />}
-				{!sidebar && <Sidebar active={setSidebar} />}
-			</MobileMenu>
-			<h1>
-				<HiOutlineGlobeAlt /> Site para sua Marca
-			</h1>
+			<div>
+				<h1>
+					<HiOutlineGlobeAlt /> Site para sua Marca
+				</h1>
+			</div>
 			<NavbarItems>
 				<ul>
 					<li>
-						<NavLink to="/projects">Projetos</NavLink>
+						<a href="#services">Nossos serviços</a>
 					</li>
 					<li>
 						<a href="#why">Peça o seu</a>
@@ -35,10 +32,14 @@ const Navbar = () => {
 						<a href="#how">Como funciona</a>
 					</li>
 					<li>
-						<a href="#contact">Contate-nos</a>
+						<a href="#contact">Contato</a>
 					</li>
 				</ul>
 			</NavbarItems>
+			{/* <MobileMenu>
+				{sidebar ? <RxHamburgerMenu size={30} onClick={showSidebar}/> : <GrFormClose size={40} onClick={showSidebar} />}
+				{!sidebar && <Sidebar active={setSidebar} />}
+			</MobileMenu> */}
 		</NavbarContainer>
 	);
 };
