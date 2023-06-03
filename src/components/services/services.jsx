24587fb85a26693505ae530b14a548c1370cf6fa
Marcus Sandi <tradedomarcus@gmail.com>
2023-06-03 12:02:21 -0300

    feat: add service content

---
+++ src/components/services/services.jsx
@@ -0,0 +1,20 @@
+import { ServicesContent } from "./services.styles";
+
+const Services = () => {
+	return <ServicesContent id="services">
+        <h1>Nossos Serviços</h1>
+        <h2>Sites Institucionais</h2>
+        <p>Crie uma presença online para sua empresa com um site institucional profissional. Apresente seus serviços, histórico, equipe e informações de contato de forma atraente e informativa.</p>
+        <h2>Lojas Virtuais</h2>
+        <p>Venda seus produtos online com uma loja virtual personalizada. Integre pagamentos, gerencie estoques e exiba seus produtos de maneira atraente para alcançar o sucesso nas vendas.</p>
+        <h2>Portfólios Criativos</h2>
+        <p>Se você é um artista, designer, fotógrafo ou qualquer profissional criativo, um site de portfólio é essencial para exibir seu trabalho de maneira impactante. Deixe sua criatividade brilhar e impressione seus clientes.</p>
+        <h2>Blogs Profissionais</h2>
+        <p>Compartilhe seus conhecimentos, experiências e opiniões com o mundo através de um blog profissional. Obtenha uma plataforma personalizada para publicar conteúdo relevante e atrair um público fiel.</p>
+        <h2>Sites Personalizados</h2>
+        <p>Se você tem uma ideia específica em mente, nossa equipe está pronta para transformá-la em realidade. Desenvolvemos sites personalizados para atender às suas necessidades exclusivas.</p>
+
+    </ServicesContent>;
+};
+
+export default Services;
