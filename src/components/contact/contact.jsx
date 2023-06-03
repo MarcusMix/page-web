8e9e33d89991b6f80f86fc92dfa8a0ef18fbfe09
Marcus Sandi <tradedomarcus@gmail.com>
2023-06-03 11:59:43 -0300

    refactor: remove button

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,7 +1,5 @@
 import {
 	ContactContainer,
-	ContactButton,
-	ButtonTop,
 	ContactContent,
 } from "./contact.styles";
 
@@ -14,13 +12,12 @@ const Contact = () => {
 				<h2>Contato</h2>
 				<p>Mande sua idéia no nosso WhatsApp!</p>
 				<FaWhatsapp size={55} />
-				<ContactButton>
-					<ButtonTop>
+		
+					
 						<a href="https://wa.me/5548996162581?text=Eu+gostaria+de+um+site+com+as+seguintes+informa%C3%A7%C3%B5es...">
 							Clique aqui
 						</a>
-					</ButtonTop>
-				</ContactButton>
+			
 			</ContactContent>
 		</ContactContainer>
 	);
