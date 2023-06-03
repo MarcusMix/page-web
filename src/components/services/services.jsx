da70db56b3d54ff09ec59bf54125591c5e41dac7
Marcus Sandi <tradedomarcus@gmail.com>
2023-06-03 14:29:34 -0300

    refactor: change content of page

--- src/components/services/services.jsx
+++ src/components/services/services.jsx
@@ -1,20 +1,79 @@
 import { ServicesContent } from "./services.styles";
 
 const Services = () => {
-	return <ServicesContent id="services">
-        <h1>Nossos Serviços</h1>
-        <h2>Sites Institucionais</h2>
-        <p>Crie uma presença online para sua empresa com um site institucional profissional. Apresente seus serviços, histórico, equipe e informações de contato de forma atraente e informativa.</p>
-        <h2>Lojas Virtuais</h2>
-        <p>Venda seus produtos online com uma loja virtual personalizada. Integre pagamentos, gerencie estoques e exiba seus produtos de maneira atraente para alcançar o sucesso nas vendas.</p>
-        <h2>Portfólios Criativos</h2>
-        <p>Se você é um artista, designer, fotógrafo ou qualquer profissional criativo, um site de portfólio é essencial para exibir seu trabalho de maneira impactante. Deixe sua criatividade brilhar e impressione seus clientes.</p>
-        <h2>Blogs Profissionais</h2>
-        <p>Compartilhe seus conhecimentos, experiências e opiniões com o mundo através de um blog profissional. Obtenha uma plataforma personalizada para publicar conteúdo relevante e atrair um público fiel.</p>
-        <h2>Sites Personalizados</h2>
-        <p>Se você tem uma ideia específica em mente, nossa equipe está pronta para transformá-la em realidade. Desenvolvemos sites personalizados para atender às suas necessidades exclusivas.</p>
-
-    </ServicesContent>;
+	return (
+		<ServicesContent id="services">
+			<h1>Nossos Serviços</h1>
+			<h2>Sites Institucionais</h2>
+			<p>
+				Criar uma presença online profissional é essencial para
+				empresas de todos os tamanhos. Com nossos sites
+				institucionais personalizados, você pode apresentar sua
+				empresa de maneira única e atraente. Nossos designs
+				exclusivos e modernos ajudam a transmitir a identidade
+				da sua marca, enquanto o conteúdo estrategicamente
+				elaborado destaca seus serviços, história, equipe e
+				informações de contato de forma clara e concisa. Através
+				de uma combinação perfeita de estética e funcionalidade,
+				garantimos que seu site institucional seja uma
+				ferramenta eficaz para atrair clientes e fortalecer sua
+				reputação online.
+			</p>
+			<h2>Lojas Virtuais</h2>
+			<p>
+				Para empresas que desejam expandir seus negócios e
+				alcançar um público global, oferecemos soluções de
+				e-commerce sob medida. Nossas lojas virtuais
+				personalizadas são projetadas para impulsionar as vendas
+				e proporcionar uma experiência de compra excepcional aos
+				seus clientes. Integramos sistemas de pagamento seguros,
+				gerenciamento de estoque eficiente e recursos avançados
+				de pesquisa e filtragem de produtos. Além disso, focamos
+				na criação de interfaces intuitivas e responsivas,
+				permitindo que seus clientes naveguem facilmente pelos
+				produtos e concluam suas compras com confiança. Com uma
+				loja virtual desenvolvida pela nossa equipe, você estará
+				pronto para o sucesso no comércio eletrônico.
+			</p>
+			<h2>Portfólios Criativos</h2>
+			<p>
+				Se você é um profissional criativo, como um artista,
+				designer, fotógrafo ou escritor, nosso serviço de
+				criação de portfólios criativos é perfeito para você.
+				Combinamos um design elegante e contemporâneo com
+				recursos avançados de apresentação de obras de arte,
+				projetos ou textos. Seu portfólio será uma vitrine
+				impressionante do seu talento e criatividade, permitindo
+				que você conquiste novas oportunidades e atraia clientes
+				em potencial. Personalizamos cada detalhe do seu
+				portfólio para refletir sua visão artística única,
+				garantindo que ele se destaque no cenário competitivo
+				atual.
+			</p>
+			<h2>Blogs Profissionais</h2>
+			<p>
+				Compartilhar conhecimentos e experiências por meio de um
+				blog profissional é uma maneira poderosa de construir
+				autoridade em um determinado nicho e envolver sua
+				audiência. Nossa equipe especializada em desenvolvimento
+				de blogs cria plataformas personalizadas que são fáceis
+				de usar e ricas em recursos. Adaptamos o design do seu
+				blog às suas preferências e garantimos uma experiência
+				de leitura agradável e intuitiva. Seja você um
+				especialista em tecnologia, moda, alimentação ou
+				qualquer outro campo, nosso serviço de criação de blogs
+				profissionais ajudará a posicionar você como uma
+				referência em seu setor.
+			</p>
+			<h2>Sites Personalizados</h2>
+			<p>
+				Se você tem uma visão única para o seu site ou precisa
+				de recursos específicos que não são atendidos pelos
+				nossos serviços pré-existentes, nossa equipe está pronta
+				para criar um site personalizado
+			</p>
+		</ServicesContent>
+	);
 };
 
 export default Services;
