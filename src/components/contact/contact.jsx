4a7bcc67b329ae667348143e2c832a3e39c9147d
Marcus Sandi <tradedomarcus@gmail.com>
2023-06-03 14:30:04 -0300

    feat: add whatsapp button

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,23 +1,20 @@
-import {
-	ContactContainer,
-	ContactContent,
-} from "./contact.styles";
+import { ContactContainer, ContactContent } from "./contact.styles";
 
-import { FaWhatsapp } from "react-icons/fa";
+// import { FaWhatsapp } from "react-icons/fa";
+
+import WhatsAppImage from "../../assets/whats.png";
 
 const Contact = () => {
 	return (
 		<ContactContainer id="contact">
 			<ContactContent>
 				<h2>Contato</h2>
-				<p>Mande sua idéia no nosso WhatsApp!</p>
-				<FaWhatsapp size={55} />
-		
-					
-						<a href="https://wa.me/5548996162581?text=Eu+gostaria+de+um+site+com+as+seguintes+informa%C3%A7%C3%B5es...">
-							Clique aqui
-						</a>
-			
+				<p>Mande sua idéia ou dúvida no nosso WhatsApp!</p>
+				{/* <FaWhatsapp size={55} /> */}
+
+				<a href="https://wa.me/5548996162581?text=Eu+gostaria+de+um+site+com+as+seguintes+informa%C3%A7%C3%B5es...">
+					<img src={WhatsAppImage} alt="WhatsApp button" />
+				</a>
 			</ContactContent>
 		</ContactContainer>
 	);
