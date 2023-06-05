70644e25f75b7e89b94821ca20f004d80ab763a3
Marcus Sandi <tradedomarcus@gmail.com>
2023-06-05 16:12:09 -0300

    feat: sizes and organization of paragraph

--- src/components/why/why.styles.js
+++ src/components/why/why.styles.js
@@ -5,19 +5,47 @@ export const WhyContainer = styled.div`
 	align-items: center;
 	justify-content: center;
 	flex-direction: column;
-	height: 100vh;
-	background-color: #fff;	
+	height: 100%;
+	background-color: #fff;
 	color: #000;
 
 	& h2 {
-		font-size: 40px;
-		margin: 1rem;
-		color: #11151C;
+		font-size: 20px;
+		color: #11151c;
 		text-align: center;
+		padding: 1rem;
+		font-size: 40px;
+		color: #000;
+		margin-top: 13rem;
 	}
 
 	& h3 {
-		margin: .25rem;
 		text-align: center;
+		padding: 1rem;
+		margin: auto 10%;
+		color: #fb6107;
+	}
+
+	& p {
+		padding: 1rem;
+		margin: auto 20%;
+		text-align: justify;
+	}
+
+	@media (max-width: 1400px) {
+		height: 100%;
+
+		p {
+			font-size: 14px;
+			margin: auto 10%;
+		}
+
+		h3 {
+			font-size: 19px;
+		}
+
+		h2 {
+			margin-top: 1rem;
+		}
 	}
 `;
