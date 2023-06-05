367afcd867a7dcd8f97c2a9ce7222dbbd1f52ed4
Marcus Sandi <tradedomarcus@gmail.com>
2023-06-05 16:13:34 -0300

    feat: change configs of style

--- src/components/services/services.styles.js
+++ src/components/services/services.styles.js
@@ -19,7 +19,7 @@ export const ServicesContent = styled.div`
 	h2 {
 		font-size: 20px;
 		color: #fb6107;
-		text-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.7) 0px 30px 60px -30px;
+		padding: 1rem;
 	}
 
 	& p {
@@ -27,21 +27,30 @@ export const ServicesContent = styled.div`
 		margin: auto 20%;
 	}
 
-	@media (max-width: 1200px) {
-        height: 100%;
+	@media (max-width: 1400px) {
+		h1 {
+			font-size: 35px;
+			color: #000;
+		}
+	}
+
+
+
+	@media (max-width: 1400px) {
+		height: 100%;
 
 		h1 {
-            font-size: 30px;
-            color: #000;
-        }
+			font-size: 30px;
+			color: #000;
+		}
 
-        h2 {
-            font-size: 16px;
-        }
+		h2 {
+			font-size: 19px;
+		}
 
-        p {
-            font-size: 14px;
-            margin: auto 10%;
-        }
+		p {
+			font-size: 14px;
+			margin: auto 10%;
+		}
 	}
 `;
