21bfa454b78f15fdddf1a7af3afe46d8f8d3d70d
Marcus Sandi <tradedomarcus@gmail.com>
2023-06-07 16:10:01 -0300

    refactor: change div with display grid

--- src/components/why/why.jsx
+++ src/components/why/why.jsx
@@ -1,56 +1,73 @@
-import { WhyContainer } from "./why.styles";
+import { WhyContainer, WhyWrapper } from "./why.styles";
 
 const Why = () => {
 	return (
-		<WhyContainer id="why">
+		<WhyWrapper>
 			<h2>Por que ter um site?</h2>
+			<WhyContainer id="why">
+				<div>
+					<h3>Alcance global</h3>
+					<p>
+						Um site permite que você alcance clientes em
+						todo o mundo, sem limitações geográficas.
+						Isso amplia o potencial do seu negócio e
+						permite que você atinja um público muito
+						maior do que seria possível com uma loja
+						física.
+					</p>
+					<h3>Aumente suas vendas</h3>
+					<p>
+						Um site permite que você venda seus produtos
+						ou serviços online. Você pode configurar uma
+						loja virtual integrada ao seu site,
+						permitindo que os clientes façam compras
+						diretamente de você. Isso amplia seu alcance
+						e pode aumentar significativamente suas
+						vendas.
+					</p>
+					<h3>Ganhe visibilidade</h3>
+					<p>
+						Ter um site aumenta a exposição e a
+						visibilidade da sua marca. As pessoas podem
+						encontrar informações sobre sua empresa,
+						produtos e serviços facilmente, o que ajuda
+						a construir confiança e credibilidade.
+					</p>
+				</div>
 
-			
-				<h3>Alcance global</h3>
-				<p>
-					Um site permite que você alcance clientes em todo
-					o mundo, sem limitações geográficas. Isso amplia o
-					potencial do seu negócio e permite que você atinja
-					um público muito maior do que seria possível com
-					uma loja física.
-				</p>
-				<h3>Aumente suas vendas</h3>
-				<p>
-					Um site permite que você venda seus produtos ou
-					serviços online. Você pode configurar uma loja
-					virtual integrada ao seu site, permitindo que os
-					clientes façam compras diretamente de você. Isso
-					amplia seu alcance e pode aumentar
-					significativamente suas vendas.
-				</p>
-				<h3>Ganhe visibilidade</h3>
-				<p>
-					Ter um site aumenta a exposição e a visibilidade
-					da sua marca. As pessoas podem encontrar
-					informações sobre sua empresa, produtos e serviços
-					facilmente, o que ajuda a construir confiança e
-					credibilidade.
-				</p>
-				<h3>Tenha mais credibilidade</h3>
-				<p>
-					Ter um site bem projetado e funcional transmite
-					uma imagem profissional e confiável para os
-					clientes. Isso ajuda a estabelecer credibilidade e
-					diferenciar seu negócio da concorrência expondo
-					seu trabalho ou serviço.
-				</p>
+				<div>
+					<h3>Tenha mais credibilidade</h3>
+					<p>
+						Ter um site bem projetado e funcional
+						transmite uma imagem profissional e
+						confiável para os clientes. Isso ajuda a
+						estabelecer credibilidade e diferenciar seu
+						negócio da concorrência expondo seu trabalho
+						ou serviço.
+					</p>
 
-				<h3>Disponibilidade 24 horas por dia</h3>
-				<p>
-					Ao contrário de uma loja física com horário de
-					funcionamento limitado, um site está disponível
-					para os clientes 24 horas por dia, 7 dias por
-					semana. Isso permite que as pessoas acessem seu
-					negócio a qualquer momento, mesmo quando você está
-					dormindo ou ausente.
-				</p>
-			
-		</WhyContainer>
+					<h3>Disponibilidade 24 horas por dia</h3>
+					<p>
+						Ao contrário de uma loja física com horário
+						de funcionamento limitado, um site está
+						disponível para os clientes 24 horas por
+						dia, 7 dias por semana. Isso permite que as
+						pessoas acessem seu negócio a qualquer
+						momento, mesmo quando você está dormindo ou
+						ausente.
+					</p>
+					<h3>Tenha mais credibilidade</h3>
+					<p>
+						Ter um site bem projetado e funcional
+						transmite uma imagem profissional e
+						confiável para os clientes. Isso ajuda a
+						estabelecer credibilidade e diferenciar seu
+						negócio da concorrência expondo seu trabalho
+						ou serviço.
+					</p>
+				</div>
+			</WhyContainer>
+		</WhyWrapper>
 	);
 };
 
--- src/components/why/why.styles.js
+++ src/components/why/why.styles.js
@@ -1,13 +1,8 @@
 import { styled } from "styled-components";
 
-export const WhyContainer = styled.div`
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	flex-direction: column;
-	height: 100%;
-	background-color: #fff;
-	color: #000;
+export const WhyWrapper = styled.div`
+	height: 100vh;
+	background-color: aquamarine;
 
 	& h2 {
 		font-size: 20px;
@@ -16,7 +11,21 @@ export const WhyContainer = styled.div`
 		padding: 1rem;
 		font-size: 40px;
 		color: #000;
-		margin-top: 13rem;
+		margin: 3rem;
+	}
+`;
+
+export const WhyContainer = styled.div`
+	display: grid;
+	grid-template-columns: repeat(2, 1fr);
+	grid-gap: 10px;
+	background-color: #fff;
+	margin: auto 17%;
+	color: #000;
+
+	& div {
+		background-color: darkorchid;
+		/* height: 300px; */
 	}
 
 	& h3 {
@@ -28,16 +37,29 @@ export const WhyContainer = styled.div`
 
 	& p {
 		padding: 1rem;
-		margin: auto 20%;
+		margin: auto 10%;
 		text-align: justify;
 	}
 
 	@media (max-width: 1400px) {
+		grid-template-columns: repeat(2, 1fr);
 		height: 100%;
+
+		margin: auto 8%;
+
+		p {
+			margin: auto 7%;
+		}
+	}
+
+	@media (max-width: 768px) {
+		height: 100%;
+		grid-template-columns: repeat(1, 1fr);
+		margin: auto 5%;
 
 		p {
 			font-size: 14px;
-			margin: auto 10%;
+			margin: auto 7%;
 		}
 
 		h3 {
